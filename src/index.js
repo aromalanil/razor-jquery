4bbffe9783da4de103f4cabf44bb1e8e2aba44da
Aromal Anil <aromal.anil@razorpay.com>
2021-08-06 13:13:37 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/**
+ * Represents a list of DOM elements
+ * with extra utility methods 
+ */
+class RazorNodeList {
+  constructor(nodeList) {
+    this.nodeList = nodeList;
+  }
+
+  /**
+   * A private method to iterate over each node in the list
+   * and execute a callback
+   * @param {Function} callback Callback to be called on each node
+   */
+  #forEachNode(callback) {
+    this.nodeList.forEach(callback);
+  }
+
+  /**
+   * 
+   * @param {Object} style A style object with property names and its corresponding value
+   * @returns The list of DOM elements with updated style
+   */
+  setStyle(style) {
+    for (const [key, value] of Object.entries(style)) {
+      this.#forEachNode((node) => {
+        node.style[key] = value;
+      });
+    }
+    return this;
+  }
+}
+
+/**
+ * 
+ * A utility function similar to Jquery
+ * @param {String} selector The query to select the dom elements (Must be a valid CSS Selector)
+ * @returns All elements matching the selector
+ */
+function $razor(selector) {
+  const nodeList = document.querySelectorAll(selector);
+  return new RazorNodeList(nodeList);
+}
